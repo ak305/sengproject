784adcc36fc4aa69960a283697d192b7d759216d
William Guo <williamg2103@gmail.com>
2015-05-14 17:01:26 +1000

    Payed out buttons and combo boxes onto sidebar

--- GUI/Sidebar.java
+++ GUI/Sidebar.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.util.ArrayList;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 
 
 public class Sidebar extends JPanel {
@@ -50,27 +51,30 @@ public class Sidebar extends JPanel {
 			date.add(new JComboBox());
 		}
 		time = new ArrayList<JComboBox>();
+		timeLabel = new JLabel();
 		for(int i = 0; i < 2; i++){
 			time.add(new JComboBox());
 		}
 		airline = new JComboBox();
+		airlineLabel = new JLabel();
 		numOutput = new JTextField();
+		numOutputLabel = new JLabel();
 		search = new JButton("Search");
 		clear = new JButton("Clear");
 		
 		
-		add(startCity);
-		add(endCity);
-		for(JComboBox box : date){
-			add(box);
-		}
-		for(JComboBox box : time){
-			add(box);
-		}
-		add(airline);
-		add(numOutput);
-		add(search);
-		add(clear);
+//		add(startCity);
+//		add(endCity);
+//		for(JComboBox box : date){
+//			add(box);
+//		}
+//		for(JComboBox box : time){
+//			add(box);
+//		}
+//		add(airline);
+//		add(numOutput);
+//		add(search);
+//		add(clear);
 		
 		display();
 		// align elements
@@ -78,39 +82,146 @@ public class Sidebar extends JPanel {
 	}
 	
 	private void display(){
+		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		startCityLabel.setText("City Start: ");
 		endCityLabel.setText("City End: ");
 		dateLabel.setText("Flight Date: ");
-
+		timeLabel.setText("Flight Time: ");
+		airlineLabel.setText("Airline: ");
+		numOutputLabel.setText("No. Routes ");
+		
+		JPanel container = new JPanel();
+		container.setPreferredSize(new Dimension(320, 400));
+		container.setBackground(Color.LIGHT_GRAY);
+		container.setLayout(new GridBagLayout());
+		add(container, BorderLayout.NORTH);
+		GridBagConstraints c = new GridBagConstraints();
+		c.anchor = GridBagConstraints.NORTH;
+		c.insets = new Insets(15,0,0,0);
+//		c.weighty = 1.0;
+		
+		// flight date - Gridy 2
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 2;
+		container.add(dateLabel, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 1;
+		c.gridy = 2;
+		container.add(date.get(0), c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 2;
+		c.gridy = 2;
+		container.add(date.get(1), c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 3;
+		c.gridy = 2;
+		container.add(date.get(2), c);
+		
+		//time gridy 3
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 3;
+		container.add(timeLabel, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 1;
+		c.gridy = 3;
+		container.add(time.get(0), c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 2;
+		c.gridy = 3;
+		container.add(time.get(1), c);
+		
+		// clear grid 7
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 7;
+		container.add(clear, c);
+		
+		// search grid 7
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 2;
+		c.gridy = 7;
+		c.gridwidth = 2;
+		container.add(search, c);
+		
+		// startCity gridy 0
+        c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 0;
+//		c.anchor = GridBagConstraints.NORTH;
+		container.add(startCityLabel, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 1;
+		c.gridy = 0;
+		c.gridwidth = 3;
+		container.add(startCity, c);
+		
+		//end city gridy 1
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 1;
+		container.add(endCityLabel, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 1;
+		c.gridy = 1;
+		c.gridwidth = 3;
+		container.add(endCity, c);
+		
+		//airline grid 4
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 4;
+		container.add(airlineLabel, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 1;
+		c.gridy = 4;
+		c.gridwidth = 3;
+		container.add(airline, c);
+		
+		// priority gridy 5
+		
+		// numOutput gridy 6
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 6;
+		container.add(numOutputLabel, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 1;
+		c.gridy = 6;
+		c.gridwidth = 3;
+		container.add(numOutput, c);
 		
 		
 		
-		GroupLayout layout = new GroupLayout(this);
-		this.setLayout(layout);
-		layout.setAutoCreateContainerGaps(true);
-		layout.setAutoCreateGaps(true);
-		layout.setHorizontalGroup(layout.createSequentialGroup()
-			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING))
-			    	.addComponent(startCityLabel)
-			    	.addComponent(startCity)
-			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING))
-			    	.addComponent(endCityLabel)
-			    	.addComponent(endCity)
-		);
 		
-		layout.setVerticalGroup(layout.createSequentialGroup()
-			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE))
-			    	.addComponent(startCityLabel)
-			    	.addComponent(endCityLabel)
-			    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING))
-			    	.addComponent(startCity)
-			    	.addComponent(endCity)
-		);
-
-				 
 		
-		startCity.setPreferredSize(new Dimension(50,50));
-//		numOutput.setSize(50, 50);
 	}
 	
 	
